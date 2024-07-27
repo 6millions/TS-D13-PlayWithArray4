ed2d06d6f8d896857dbb8be862689b519de00409
6millions <kittichai.s@ic-connection.com>
2024-07-27 16:30:24 +0700

    finish practice for today

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# TS-Challenge-Template
-Template of TS Challenge
+# TS-D13-PlayWithArray4
+Practice Array In TypeScript
+
+## Overview
+Play with map function in array - Specialize in Aliases type
+
+## Installation
+1. Clone the repository
+2. Install dependencies: npm install
+
+## Problem Explanation
+1. Define type by simple 'type' keyword
+2. Play with array by converting from string to number
+3. Return which list is greater
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+# Special Note
+Include `launch.json` to version control, for the purpose of this project and as a learning resource for debugging TypeScript projects with Visual Studio Code.
--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,45 @@ app.use(bodyParser.json());
 
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript with Express!');
+
+    const msg = compareTotalExp(Jack.expList, John.expList);
+
+    res.send(msg);
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+type Person = { name: string, age: number, travelPlace: string[], expList: string[] };
+
+let Jack: Person = { name: "Jack", age: 30, travelPlace: ["A", "B", "C"], expList: ["10", "20", "30"] };
+let John: Person = { name: "John", age: 28, travelPlace: ["E", "F", "G"], expList: ["10", "2", "3", "x"] };
+
+function compareTotalExp(input1: string[], input2: string[]): string {
+    console.log("[compareTotalExp]");
+
+    // convert string array to 
+
+    const numList1 = input1.map(Number);
+    const numList2 = input2.map(Number);
+
+    console.log(numList1);
+    console.log(numList2);
+
+    // filter , use for filter NOT Number out, we can do in several way to protect the error but this is only test another function of array.
+    const totalNumList1 = numList1.filter(value => !Number.isNaN(value)).reduce((prev, current) => prev + current, 0);
+    const totalNumList2 = numList2.filter(value => !Number.isNaN(value)).reduce((prev, current) => prev + current, 0);
+
+    console.log('total list1: ' + totalNumList1);
+    console.log('total list2: ' + totalNumList2);
+
+
+    if (totalNumList1 > totalNumList2) {
+        return "list 1 > list 2"
+    }
+    else {
+        return "list 2 > list 1"
+    }
+
+
+}
